be64019ff8bc2205f4ab8fec1e26dee1c1210016
Hoxton <hoxton019030@gmail.com>
2024-07-31 16:56:07 +0800

    log

--- dubbo-consumer/src/main/java/com/example/dubboconsumer/controller/HelloController.java
+++ dubbo-consumer/src/main/java/com/example/dubboconsumer/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.example.dubboconsumer.controller;
 
 import com.example.dubboprovider.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @DubboReference
@@ -13,6 +15,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String getHello(){
+        log.info("呼叫控制器");
         return helloService.sayHello();
     }
 }
--- dubbo-provider/src/main/java/com/example/dubboprovider/HelloServiceImpl.java
+++ dubbo-provider/src/main/java/com/example/dubboprovider/HelloServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.dubboprovider;
 
 import com.example.dubboprovider.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
 @DubboService
+@Slf4j
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello() {
+        log.info("說Hello");
         return "Hello Dubbo";
     }
 }
